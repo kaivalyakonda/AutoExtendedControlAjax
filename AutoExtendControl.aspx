c329b27421fac2bf052c39155afb390433ab71d7
kaivalyakonda <kondasowmya@gmail.com>
2014-08-01 14:42:27 -0700

    Auto Extender Control Using SQL

---
+++ AutoExtendControl.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AutoExtendControl.aspx.cs" Inherits="AutoExtendControl" %>
+
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit" tagprefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 92px;
+        }
+        .auto-style3 {
+            width: 92px;
+            height: 26px;
+        }
+        .auto-style4 {
+            height: 26px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
+        </asp:ToolkitScriptManager>
+    
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style3">Name</td>
+                <td class="auto-style4">
+                    <asp:TextBox ID="txtStudentName" runat="server"></asp:TextBox>
+                    <asp:AutoCompleteExtender ID="txtStudentName_AutoCompleteExtender" runat="server" CompletionSetCount="1" DelimiterCharacters="" Enabled="True" TargetControlID="txtStudentName" ServiceMethod="GetNamesList" CompletionInterval="10" MinimumPrefixLength="1">
+                    </asp:AutoCompleteExtender>
+                    <asp:Label ID="Label1" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnCheck" runat="server" OnClick="btnCheck_Click" Text="Check" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ AutoExtendControl.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class AutoExtendControl : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        /*Data Source=RAGHU\SQLEXPRESS;Initial Catalog="Sangam Trust";Integrated Security=True*/
+
+    }
+
+
+    [System.Web.Script.Services.ScriptMethod()]
+    [System.Web.Services.WebMethod]
+    public static List<string> GetNamesList(string prefixText)
+    {
+        SqlConnection con = new SqlConnection("Data Source=RAGHU\\SQLEXPRESS;Initial Catalog=Sangam Trust;Integrated Security=True");
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from StudentData where StudentName like @StudentName+'%'",con);
+        cmd.Parameters.AddWithValue("@StudentName", prefixText);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+        List<string> StudentNames = new List<string>();
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            StudentNames.Add(dt.Rows[i][1].ToString());
+        }
+        return StudentNames;
+    }
+    protected void btnCheck_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection("Data Source=RAGHU\\SQLEXPRESS;Initial Catalog=Sangam Trust;Integrated Security=True");
+        con.Open();
+        // SqlCommand myCommand = new SqlCommand("select * from customer_registration where username='" + Session["username"] + "'", con1);
+
+        SqlCommand cmd = new SqlCommand("select * from StudentData where StudentName='"+txtStudentName.Text+"'",con);
+        
+        //cmd.CommandText = "select * from StudentData where StudentName=" + txtStudentName.Text;
+        //cmd.Connection = con;
+        SqlDataReader rdr;
+        rdr = cmd.ExecuteReader();
+        bool temp = false;
+        while (rdr.Read())
+        {
+            temp = true;
+        }
+        if (temp == true)
+        {
+
+            //MessageBox.Show("Name Exist");
+            Label1.Text = "Name Exists";
+        }
+        else
+        {
+            Label1.Text = "Name doesn't Exists";
+        }
+    }
+}
